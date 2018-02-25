b89f52ade547b27aea8194548e9555e0f7958681
joshrhughes <josh@mtnpine.us>
2018-02-25 09:40:03 -0700

    added spinner component

---
+++ auth/src/components/common/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'reach-native';
+
+const Spinner = () => {
+    return (
+        <View />
+    );
+}
+
+export { Spinner };
--- auth/src/components/common/index.js
+++ auth/src/components/common/index.js
@@ -5,4 +5,5 @@ export * from './Card';
 export * from './CardSection';
 export * from './Header';
 export * from './Input';
+export * from './Spinner';
 
