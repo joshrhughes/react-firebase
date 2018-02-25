7cc4007a518bf812a03e7d4e7214700a798d9183
joshrhughes <josh@mtnpine.us>
2018-02-24 21:29:43 -0700

    added Input component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -3,14 +3,27 @@
 //going to be a class based component
 
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+    //add state to recive input from user
+    state = { text: ''};
+
+
+    //TextInput -> user types text -> onChange Text event called -> 'setstate' with new text -> component rerenders 
+    // when TextInput rendere, we tell it that its value is this.state.text
+
     render(){
         return ( 
             <Card>
-                <CardSection />
+                <CardSection>
+                    <TextInput 
+                    value={this.state.text}
+                    onChangeText={text => this.setState({ text })}
+                    style={{ height: 20, width: 100}}/>
+                    
+                </CardSection>    
                 <CardSection />
                 <CardSection>
                     <Button>
